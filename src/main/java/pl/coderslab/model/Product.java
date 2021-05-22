5cc48f6aa6a4de946872fd18365e67dc5bf51796
Michał <wrobel239@gmail.com>
2021-05-22 14:30:53 +0200

    konfiguracja projektu (działa) + próbna pierwsza encja + próbny pierwszy kontroler

---
+++ src/main/java/pl/coderslab/model/Product.java
@@ -0,0 +1,134 @@
+package pl.coderslab.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // to ma być w ShoppingCart
+//    @Column(name = "session_id")
+//    private String sessionId;
+    private String name;
+
+    @Column(scale=2, precision = 11)
+    private BigDecimal price;
+
+    @Column(name = "is_available")
+    private boolean isAvailable;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "src_image")
+    private String srcImage;
+
+    @Column(name = "alt_image")
+    private String altImage;
+
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+    @PrePersist
+    public void prePersist(){
+        created = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updated = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSrcImage() {
+        return srcImage;
+    }
+
+    public void setSrcImage(String srcImage) {
+        this.srcImage = srcImage;
+    }
+
+    public String getAltImage() {
+        return altImage;
+    }
+
+    public void setAltImage(String altImage) {
+        this.altImage = altImage;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", isAvailable=" + isAvailable +
+                ", description='" + description + '\'' +
+                ", srcImage='" + srcImage + '\'' +
+                ", altImage='" + altImage + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
